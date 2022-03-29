4ec36bccc1929f1bbfed68e3318baad9174634d5
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-03-29 22:43:23 +0500

    added services folder with custom api url functions base url and added a package axios

---
+++ services/custom.js
@@ -0,0 +1,7 @@
+export const required = val => val && val.length;
+export const maxLength = len => val => !val || val.length <= len;
+export const minLength = len => val => val && val.length >= len;
+export const validEmail = val =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+export const ImageURL =
+  'https://dev73.myprojectstaging.com/AwtbAppareal/public/documents/';
